8ba0b31644e6468e6cc956d2457ecde3e7fc6cdd
Alain Guillon <zyrass@outlook.fr>
2025-11-16 10:40:15 +0100

    core: remove script mermaid

--- docs/assets/javascripts/mermaid-config.js
+++
@@ -1,9 +0,0 @@
-// Configuration globale Mermaid pour Zensical
-// Objectif : désactiver les web workers pour corriger le rendu sur Chrome/Brave mobile.
-window.mermaid = {
-  startOnLoad: true,
-  securityLevel: "strict",
-  theme: "default",
-  // Le point important : désactiver les workers
-  workerEnabled: false
-};
